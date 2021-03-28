86093a07f7f426569308cc17c9a8ac93c0529fd0
lingdev1998 <linhdeptrai1029i@gmail.com>
2021-03-29 01:12:03 +0700

    add:Dockerfile

---
+++ README.md
@@ -0,0 +1,30 @@
+#Music App
+
+
+Back-end side of music app project with Gradle dependencies manager. 
+
+
+Technologies: 
+- Spring Boot
+- Spring Cloud
+- RabbitMQ
+- Eureka Discovery 
+- Zuul Gateway
+- Spring Security
+- Spring Web
+- Docker
+- MySQL
+- Flyway
+- Lombok
+- Hibernate
+- Rest Repositories  
+
+<img src="https://i.ibb.co/PDqFcb1/nh-edit-new-768x345.png" 
+     style="display:block;float:none;margin-left:auto;margin-right:auto;width:60%" alt="Spring Cloud"/> 
+ 
+  
+<figure class="image" >
+  <img src="https://i.ibb.co/7j0tQw0/routing-requests-spring-cloud-netflix-zuul.png" alt="Services Architect"> 
+</figure>
+
+
---
+++ account-service/src/main/java/com/music/backend/controller/AccountController.java
@@ -0,0 +1,19 @@
+package com.music.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+        // This is useful for debugging
+        // When having multiple instance of gallery service running at different ports.
+        // We load balance among them, and display which instance received the request.
+        return "Hello from Gallery Service running at port: ${server.port}";
+    }
+
+}
